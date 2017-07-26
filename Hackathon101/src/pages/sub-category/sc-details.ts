0813b52298093be6db09e48fb15e5c498a9aeba1
FNU Ananya <t-aanany@microsoft.com>
2017-07-26 08:08:09 +0000

    Merged PR 14: Merge ananya3 to dev
    Optimized the L2 code to remove multiple item arrays

--- Hackathon101/src/pages/sub-category/sc-details.ts
+++ Hackathon101/src/pages/sub-category/sc-details.ts
@@ -10,9 +10,7 @@ import { ItemDetailsPage } from '../item-details/item-details';
 })
 export class SubCategoryPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string, translation: string}>;
-  items1: Array<{title: string, note: string, icon: string, translation: string}>;
-  items2: Array<{title: string, note: string, icon: string, translation: string}>;
+  itemRows: Array<Array<{title: string, note: string, icon: string, translation: string}>>;
   selectedItem: any;
   categories: string[];
   translations: string[];
@@ -21,36 +19,25 @@ export class SubCategoryPage {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     
-    this.icons = ['cart', 'call', 'bus', 'wine', 'car', 'knife'];
-    this.categories = ['Shopping','Emergency','Transportation','Food','Driving','Kitchen']
-    this.translations = ['تسوق', 'طوارئ', 'تنقل', 'طعام', 'القيادة', 'المطبخ']
+    this.icons = ['cart',  'call', 'cart', 'bus', 'wine', 'car'];
+    this.categories = ['Kitchen', 'Emergency','Shopping', 'Transportation','Food','Driving']
+    this.translations = [ 'المطبخ', 'طوارئ', 'تسوق', 'تنقل', 'طعام', 'القيادة']
 
-    this.items = [];
-    this.items1 = [];
-    this.items2 = [];
-    for(let i = 0; i < this.icons.length; i++) {
-      this.items.push({
+    this.itemRows = [];
+
+    for(let i = 0; i < this.icons.length - 1; i+=2) {
+      this.itemRows.push([{
         title: this.categories[i],
         note: '',
         icon: this.icons[i],
         translation: this.translations[i]
-      });
-      if(i%2==0){
-        this.items1.push({
-          title: this.categories[i],
-          note: '',
-          icon: this.icons[i],
-          translation: this.translations[i]
-        });
-      }
-      else {
-        this.items2.push({
-          title: this.categories[i],
-          note: '',
-          icon: this.icons[i],
-          translation: this.translations[i]
-        });
-      }
+      },
+      {
+        title: this.categories[i+1],
+        note: '',
+        icon: this.icons[i+1],
+        translation: this.translations[i+1]
+      }]);
     }
   }
 
